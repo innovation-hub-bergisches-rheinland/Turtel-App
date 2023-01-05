3fca3c573ae8ef7f4cdc605ed3fdc2331f46b403
fweil <fabian.weil1994@googlemail.com>
2023-01-05 11:30:44 +0100

    Added Messages

---
+++ Turtel-App/ServerApp/Message/Domain/ChatContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Turtel_App.ServerApp.Message.Domain;
+
+//the database context class is responsible for managing the connection to the database and exposing the database tables as DbSet properties
+
+public class ChatContext : DbContext
+{
+    // public ChatContext(DbContextOptions<ChatContext> options)
+    //     : base(options) {
+    // }
+
+    public DbSet<User.Domain.User>? Users { get; set; }
+    public DbSet<ChatRoom>? ChatRooms { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public Guid SenderId { get; set; }
+        public Guid Receiver { get; set; }
+        
+        public string Text { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+    
+    
+}
---
+++ Turtel-App/ServerApp/Message/Domain/HubConnection.cs
@@ -0,0 +1,42 @@
+﻿
+// ich glaube das ist nutzlos !!!!!!!!!!!
+// namespace Turtel_App.ServerApp.Message.Domain;
+//
+// public class HubConnection
+// {
+//     private HubConnection hubConnection;
+//
+//     public async Task ConnectAsync()
+//     {
+//         hubConnection = new HubConnectionBuilder()
+//             .WithUrl("https://yourserver.com/chathub")
+//             .Build();
+//
+//         // Start the connection
+//         await hubConnection.StartAsync();
+//     }
+//
+// // Send a message to the server
+//     private async void SendMessage(string message)
+//     {
+//         try
+//         {
+//             // Call the SendMessage method on the server
+//             await hubConnection.InvokeAsync("SendMessage", message);
+//         }
+//         catch (Exception ex)
+//         {
+//             Console.WriteLine(ex.Message);
+//         }
+//     }
+//
+// // Receive a message from the server
+//     private async void ReceiveMessage(string sender, string message)
+//     {
+//         // Display the message in the chat window
+//         chatWindow.AppendText($"{sender}: {message}\n");
+//     }
+//     
+// }
+//
+// /
